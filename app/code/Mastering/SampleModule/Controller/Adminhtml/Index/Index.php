9e794b80691f2493909f5b5fe2f0c1c889ec542d
Andrew Dallow <andrewdallow@gmail.com>
2018-06-01 17:45:42 +1200

    Fixed Admin Route

--- app/code/Mastering/SampleModule/Controller/Adminhtml/Index/Index.php
+++ app/code/Mastering/SampleModule/Controller/Adminhtml/Index/Index.php
@@ -14,7 +14,7 @@
  * @since      Class available since Release 1.0
  */
 
-namespace Mastering\SampleModule\Controller\Index;
+namespace Mastering\SampleModule\Controller\Adminhtml\Index;
 
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\App\Action\Action;
