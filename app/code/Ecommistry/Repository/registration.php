98018a19a402f145c7dd50d2784fb008ce428ced
Andrew Dallow <andrewdallow@gmail.com>
2018-06-28 12:20:09 +1200

    Added Repository Module

---
+++ app/code/Ecommistry/Repository/registration.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Short description for file
+ *
+ * Long description for file (if any)...
+ *
+ * @category   Ecommistry
+ * @package    Ecommistry_Repository
+ * @copyright  Copyright (c) 2018 ecommistry (http://www.ecommistry.com)
+ * @license    http://framework.zend.com/license   BSD License
+ * @version    Release: 1.0
+ * @link       http://framework.zend.com/package/PackageName
+ * @since      Class available since Release 1.0
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Ecommistry_Repository',
+    __DIR__
+);
